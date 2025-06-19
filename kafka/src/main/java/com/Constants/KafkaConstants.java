8fc963a1b4eda53a959e48348e6c60304ce3b04e
Afraa Shaikh <shaikhafraabi@gmail.com>
2025-06-19 13:32:12 +0530

    fix

--- kafka/src/main/java/com/Constants/KafkaConstants.java
+++ kafka/src/main/java/com/Constants/KafkaConstants.java
@@ -5,3 +5,8 @@ public class KafkaConstants {
     public static final String GROUP_ID = "kafka-sandbox";
     public static final String KAFKA_BROKER = "localhost:9092";
 }
+
+// This class contains constants used for Kafka configuration in the application.
+// KAFKA_TOPIC: The topic to which messages will be published and from which they will be consumed.
+// GROUP_ID: The consumer group ID for Kafka consumers.
+// KAFKA_BROKER: The address of the Kafka broker to connect to.
